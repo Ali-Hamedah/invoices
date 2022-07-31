e619835d96ee98f63ea96b9bd24928887eea67db
ali.hamedah <aliali735522@gmail.com>
2022-07-31 22:41:02 +0200

    =d

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers;
 
 
 use App\invoices;
-use App\User;
 use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Support\Facades\DB;
 
 
 class HomeController extends Controller
@@ -28,14 +26,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-
-        $users = User::select(
-            DB::raw("COUNT(*) as count"),
-            DB::raw("MONTHNAME(created_at) as month_name"),
-        )
-            ->whereYear('created_at', date('y'))
-            ->groupBy('month_name')
-            ->get()->toArray();
 
 
 //=================احصائية نسبة تنفيذ الحالات======================
